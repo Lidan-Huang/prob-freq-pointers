71d7dbaf89a6dfa921df6aa2ee966ff6a2fe6123
Lidan-Huang <huangld0314@gmail.com>
2022-01-14 16:37:33 -0800

    finished two-array-object fn

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,24 @@
+"use strict";
+
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keyArr, valueArr) {
+    let keyToValue = {};
+
+    if(keyArr.length <= valueArr.length) {
+        for(let i=0; i<keyArr.length; i++) {
+            keyToValue[keyArr[i]] = valueArr[i];
+        }
+    }
+
+    if(keyArr.length > valueArr.length) {
+        for(let i=0; i < valueArr.length; i++){
+            keyToValue[keyArr[i]] = valueArr[i];
+        }
+
+        for(let j=valueArr.length; j<keyArr.length; j++){
+            keyToValue[keyArr[j]] = null;
+        }
+    }
+
+    return keyToValue;
 }

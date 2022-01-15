0e113247d4f9e46f425ab81da8433b3178f6a92d
Lidan-Huang <huangld0314@gmail.com>
2022-01-14 16:00:19 -0800

    finished canConstructWord fn

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+function canConstructWord(word, letters) {
+    let freqWord = countFreq(word);
+    let freqLetters = countFreq(letters);
+
+    for(let key of freqWord.keys()) {
+        if(!(freqLetters.has(key) && (freqWord.get(key) <= freqLetters.get(key)))){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+function countFreq(characters) {
+    let freq = new Map();
+    for(let char of characters) {
+        let charCount = freq.get(char) || 0;
+        freq.set(char, charCount + 1);
+    }
+    return freq;
 }

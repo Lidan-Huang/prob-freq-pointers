245e813c1aabdbc769b6b6a2f57889e0ea89f07f
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-01-14 16:20:15 -0800

    solved the second problem and added use strict to both

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,32 @@
+"use strict";
+
+
 // add whatever parameters you deem necessary
-function averagePair() {
+// sorted
+
+
+function averagePair(sortedInts, targetAvg) {
+    let left = 0;
+    let right = sortedInts.length - 1;
+
+
+    while (left < right) {
+
+        let workingAvg = ((sortedInts[left] + sortedInts[right]) / 2);
+
+        if (workingAvg === targetAvg) {
+            return true;
+        }
+
+        else if (workingAvg < targetAvg) {
+            left++;
+        }
+
+        else {
+            right--;
+        }
+
+
+    }
+    return false;
 }

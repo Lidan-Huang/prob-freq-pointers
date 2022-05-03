2019608eecb19da9eae1e31a113fce41e0d468d5
Lidan-Huang <huangld0314@gmail.com>
2022-05-03 14:57:40 -0700

    cleaned average-pair.js file.

--- average-pair.js
+++ average-pair.js
@@ -1,25 +1,21 @@
 "use strict";
 
-/** Receives a sorted array of ints and a target average. Returns true if the sorted array contains a pair of values
- * that equal the target when averaged. Otherwise, returns false.
+/** Receives a sorted array of ints and a target average. 
+ *  Returns true if the sorted array contains a pair of values
+ *  that equal the target when averaged. Otherwise, returns false.
  */
 function averagePair(sortedInts, targetAvg) {
     let left = 0;
     let right = sortedInts.length - 1;
 
     while (left < right) {
-
         const workingAvg = ((sortedInts[left] + sortedInts[right]) / 2);
 
         if (workingAvg === targetAvg) {
             return true;
-        }
-
-        else if (workingAvg < targetAvg) {
+        } else if (workingAvg < targetAvg) {
             left++;
-        }
-
-        else {
+        } else {
             right--;
         }
 

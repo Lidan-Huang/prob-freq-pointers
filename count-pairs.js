3297bde1a798777a8571bc821b9c2defaee66098
Lidan-Huang <huangld0314@gmail.com>
2022-05-03 21:43:07 -0700

    finished count-pairs function and passed all the tests.

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,26 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+"use strict";
+
+/**Accepts two parameters, an array of distinct integers and a target number
+ * return : number of pairs that the sum is equal to target number: like
+ * ([10, 4, 8, 2, 6, 0], 10) => 3
+ */
+function countPairs(nums, target) {
+  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+  let count = 0;
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+    if (sum === target) {
+      count++;
+      left++;
+      right--;
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return count;
 }

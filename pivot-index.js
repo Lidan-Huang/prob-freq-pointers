7e95d5e2f6c8eb485f7442413ceb4eb5dd92cc46
Lidan-Huang <huangld0314@gmail.com>
2022-05-03 22:38:57 -0700

    finished pivot-index function and passed all the tests.

--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary
-function pivotIndex() {
+"use strict";
+
+/**Accepts an array of integers
+ * return: the smallest pivot index where the left side sum is equal to the right side sum
+ * otherwise return -1, like:
+ * [1,2,1,6,3,1] => 3
+ * [5,2,7] => -1
+ * [-1,3,-3,2] => 1
+ */
+
+function pivotIndex(nums) {
+  let rightSum = nums.reduce((a, b) => a + b, 0);
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    leftSum += nums[i];
+    if (leftSum === rightSum) {
+      return i;
+    }
+    rightSum -= nums[i];
+  }
+
+  return -1;
 }

d1b7165334ecf357c6883a21af58fcd4e89df148
Lidan-Huang <huangld0314@gmail.com>
2022-05-03 21:29:42 -0700

    finished is-subsequence function and passed all the tests.

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,16 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+"use strict";
+
+/**Accepts two strings, 
+ * return true, if first string is a subsquence of the second string, the same order
+ * otherwise, return false
+ */ 
+
+function isSubsequence(subsquence, str) {
+  let seqIdx = 0;
+
+  for (const letter of str) {
+    if (letter === subsquence[seqIdx]) seqIdx++;
+  }
+
+  return seqIdx === subsquence.length;
 }
